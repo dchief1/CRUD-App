c5c86d5986f7299bcd68ebb84abb3320f0099354
dchief1 <igwesiemmanuel4@gmail.com>
2022-04-25 23:00:03 +0100

    created http server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+
+const app = express()
+
+dotenv.config({path: 'config.env'})
+const PORT = process.env.PORT || 8080
+
+// log requests
+app.use(morgan('tiny'))
+
+app.get("/", (req, res)=> {
+    res.send("Crud Application")
+})
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`) })
