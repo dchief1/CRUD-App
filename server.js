13b0007b2f29e53523ee47c1d294dc7f6845a936
dchief1 <igwesiemmanuel4@gmail.com>
2022-04-26 08:48:03 +0100

    completed the views section

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 app.get("/", (req, res)=> {
-    res.send("Crud Application")
+    res.render('index')
 })
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`) })
