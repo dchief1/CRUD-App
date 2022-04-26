baa0746d82555694c0af5a87bfb4e39f98b4c87a
dchief1 <igwesiemmanuel4@gmail.com>
2022-04-26 22:05:15 +0100

    mongoDB connected

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const bodyparser = require('body-parser')
-const path = require('path')
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
 
-const app = express()
+const connectDB = require('./server/database/connection');
 
-dotenv.config({path: 'config.env'})
+const app = express();
+
+dotenv.config( { path : 'config.env'} )
+
 const PORT = process.env.PORT || 8080
 
 // log requests
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
 
 // parse request to body-parser
-app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.urlencoded({ extended : true}))
 
 // set view engine
 app.set("view engine", "ejs")
-// app.set("views", path.resolve(__dirname, "views/ejs"))
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
 // load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
@@ -27,4 +33,4 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 // load routers
 app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`) })
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
