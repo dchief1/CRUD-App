92e9acc253174013186cbc9bb8b1871f4eb2dba4
dchief1 <igwesiemmanuel4@gmail.com>
2022-04-27 00:42:24 +0100

    API created completely and tested with postman

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -32,14 +32,34 @@ exports.create = (req,res)=>{
 }
 
 // retrieve and return all users/ retrive and return a single user
-exports.find = (req,res)=>{
-    Userdb.find()
-    .then(user => {
-        res.send(user)
-    })
-    .catch(err => {
-        res.status(500).send({ message: err.message || "Error Occured while retrieving user information"})
-    })
+exports.find = (req, res)=>{
+
+    if(req.query.id){
+        const id = req.query.id;
+
+        Userdb.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({ message : "Not found user with id "+ id})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({ message: "Erro retrieving user with id " + id})
+            })
+
+    }else{
+        Userdb.find()
+            .then(user => {
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
+            })
+    }
+
+    
 }
 
 // Update a new idetified user by user id
