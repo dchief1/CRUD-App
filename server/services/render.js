03bfbf936d736463608d5c4708ce3ca06b63e80a
dchief1 <igwesiemmanuel4@gmail.com>
2022-04-27 17:27:22 +0100

    CRUD App completed

--- server/services/render.js
+++ server/services/render.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 
 exports.homeRoutes = (req, res) => {
-    // Make a get request to /api/users
+ /********************** Make a get request to /api/users **********************/ 
     axios.get('http://localhost:3000/api/users')
         .then(function(response){
             res.render('index', { users : response.data });
@@ -18,6 +18,12 @@ exports.add_user = (req, res) => {
     res.render('add_user')
 }
 
-exports.update_user = (req, res) => {
-    res.render('update_user')
+exports.update_user = (req, res) =>{
+    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+        .then(function(userdata){
+            res.render("update_user", { user : userdata.data})
+        })
+        .catch(err =>{
+            res.send(err);
+        })
 }
